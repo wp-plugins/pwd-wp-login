b1525dbc16feb512462c2e387b179c6fbbd2c080
Plateforme WP Digital <Plateforme WP Digital@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-24 22:11:45 +0000

    Initial plugin
    git-svn-id: https://plugins.svn.wordpress.org/pwd-wp-login/trunk@1098527 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pwd-wp-login.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Plugin Name: PWD WP Login
+Version: 1.0
+Plugin URI: http://www.plateformewpdigital.fr/plugins/wp-login
+Description: Modifiy your login page WordPress
+Author: Plateforme WP Digital, Kulka Nicolas
+Author URI: http://www.plateformewpdigital.fr
+Domain Path: languages
+Network: false
+Text Domain: pwd-wp-login
+ */
+
+// don't load directly
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+// Plugin constants
+define( 'PWD_LOGIN_VERSION', '1.0' );
+define( 'PWD_LOGIN_FOLDER', 'pwd-wp-login' );
+
+define( 'PWD_LOGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'PWD_LOGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+// Function for easy load files
+function _pwd_login_load_files( $dir, $files, $prefix = '' ) {
+	foreach ( $files as $file ) {
+		if ( is_file( $dir . $prefix . $file . ".php" ) ) {
+			require_once($dir . $prefix . $file . ".php");
+		}
+	}
+}
+
+// Plugin client classes
+_pwd_login_load_files( PWD_LOGIN_DIR . 'classes/', array( 'plugin' ) );
+
+add_action( 'plugins_loaded', 'init_pwd_loginplugin' );
+function init_pwd_loginplugin() {
+
+	load_plugin_textdomain( 'pwd-wp-login', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+
+	// Load client
+	new PWD_LOGIN_Plugin();
+}
